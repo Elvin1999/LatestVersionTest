93e344a8fc046dba40ecf9ef6229b1a3935b415a
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-18 11:21:30 +0400

    without formatted

--- LatestVersionTest/Program.cs
+++ LatestVersionTest/Program.cs
@@ -16,19 +16,19 @@ namespace LatestVersionTest
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                             app.UseHsts();
-                     }
+                app.UseHsts();
+            }
 
-                         app.UseHttpsRedirection();
-                         app.UseStaticFiles();
+                          app.UseHttpsRedirection();
+                          app.UseStaticFiles();
 
 
-            app.UseRouting(); 
+                          app.UseRouting();
 
-            app.UseAuthorization();
+                          app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
+                          app.MapControllerRoute(
+                              name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
