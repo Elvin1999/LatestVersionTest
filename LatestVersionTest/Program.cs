1154a538e3642dc191b3b36ec031a2d9455c3842
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-18 11:44:28 +0400

    done 3

--- LatestVersionTest/Program.cs
+++ LatestVersionTest/Program.cs
@@ -16,10 +16,12 @@ namespace LatestVersionTest
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-           }
+                                      app.UseHsts();
+                          }
 
-             app.UseHttpsRedirection();
+                     app.UseHttpsRedirection();
+
+
             app.UseStaticFiles();
 
 
