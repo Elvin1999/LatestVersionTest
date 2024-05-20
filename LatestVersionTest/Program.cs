68839c9de7dd276c0253a0ac1cc2c83bb57ed723
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-20 09:56:57 +0400

    test

--- LatestVersionTest/Program.cs
+++ LatestVersionTest/Program.cs
@@ -2,7 +2,7 @@ namespace LatestVersionTest
 {
     public class Program
     {
-                         public static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -14,15 +14,15 @@ namespace LatestVersionTest
 
 
             // Configure the HTTP request pipeline.
-                         if (!app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            } 
+            }
 
-                   app.UseHttpsRedirection();
-                app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
 
